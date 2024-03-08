e48f64a2b159aedabe8afda72ec06d13b54ade91
dct2002 <trudang2409@gmail.com>
2024-03-08 16:34:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import './App.css';
+import Home from './pages/Home';
+import Header from './components/Layout/header';
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
+import ForgotPassword from './pages/Forgotpassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+            <Routes>
+              <Route path='/' element={<Home/ >}  />
+              <Route path='/header' element={<Header/ >}  />
+              <Route path='/login' element={<Login/ >}  />
+              <Route path='/signup' element={<SignUp/ >} />
+              <Route path='/forgotpassword' element={<ForgotPassword/ >} />
+            </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import Body from "../../components/Default/body";
+import Header from "../../components/Layout/header";
+
+function Home() {
+    return (
+        <>
+            <Header/>
+            <Body/>
+            </>
+    );
+
+}
+
+export default Home;
