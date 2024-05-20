65f0dd9b31d3cf986b9c80d1b5983498813177de
dct2002 <trudang2409@gmail.com>
2024-05-20 22:52:28 +0700

    update

--- src/pages/FormContribute/index.js
+++ src/pages/FormContribute/index.js
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import YouTubePlayer from '../../components/VideoPlayer';
+import regions from '../../assets/regions';
+import categorys from '../../assets/categorys';
 
 const FormContribute = () => {
 
@@ -23,32 +25,6 @@ const FormContribute = () => {
         source: '',
         content: [{ name: '', description: '' }]
     })
-    const regions = [
-        {
-            name: 'Bắc',
-            provinces: ['Lào Cai', 'Yên Bái', 'Điện Biên', 'Hòa Bình', 'Lai Châu', 'Sơn La', 'Hà Giang', 'Cao Bằng', 'Bắc Kạn', 'Lạng Sơn', 'Tuyên Quang', 'Thái Nguyên', 'Phú Thọ', 'Bắc Giang', 'Quảng Ninh', 'Bắc Ninh', 'Hà Nam', 'Hà Nội', 'Hải Dương', 'Hưng Yên', 'Hải Phòng', 'Nam Định', 'Ninh Bình', 'Thái Bình', 'Vĩnh Phúc']
-        },
-        {
-            name: 'Trung',
-            provinces: ['Thanh Hoá', 'Nghệ An', 'Hà Tĩnh', 'Quảng Bình', 'Quảng Trị', 'Thừa Thiên-Huế', 'Kon Tum', 'Gia Lai', 'Đắc Lắc', 'Đắc Nông', 'Lâm Đồng', 'Đà Nẵng', 'Quảng Nam', 'Quảng Ngãi', 'Bình Định', 'Phú Yên', 'Khánh Hoà', 'Ninh Thuận']
-        },
-        {
-            name: 'Nam',
-            provinces: ['An Giang', 'Bạc Liêu', 'Bà Rịa - Vũng Tàu', 'Bến Tre', 'Bình Dương', 'Bình Phước', 'Bình Thuận', 'Cà Mau', 'Cần Thơ', 'Đồng Tháp', 'Đồng Nai', 'Hậu Giang', 'Kiên Giang', 'Long An', 'Sóc Trăng', 'Tây Ninh', 'Thành phố Hồ Chí Minh', 'Tiền Giang', 'Trà Vinh', 'Vĩnh Long']
-        }
-      ];
-
-      const categorys = [
-        {
-            name: 'tangible',
-            types: ['scenic spots', 'historical sites']
-        },
-        {
-            name: 'intangible',
-            types: ['arts', 'festivals', 'customs and beliefs', 'spoken and written language', 'crafts', 'folk knowledge']
-        }
-      ];
-
 
     const handleSubmit = async(event) => {
         event.preventDefault();
